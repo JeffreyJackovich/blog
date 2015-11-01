622cc871e61351a14a0c15a1fecea3cabaa48ceb
brooklynftw <jeffrey.jackovich@gmail.com>
2015-11-01 01:55:43 +0000

    Extended the blog

---
+++ manage.py
@@ -0,0 +1,31 @@
+import os
+from flask.ext.script import Manager
+
+from blog import app
+from blog.models import Post
+from blog.database import session
+
+
+manager = Manager(app)
+
+@manager.command
+def seed():
+    content = """Lorem ipsum dolor sit amet, consectetur adipiscing elit , sunt in culpa."""
+    
+    for i in range(25):
+        post = Post(
+            title="Test Post #{}".format(i),
+            content=content
+        )
+        session.add(post)
+    session.commit()
+    
+@manager.command    
+def run():
+    port = int(os.environ.get('PORT', 8080))
+    app.run(host='0.0.0.0', port=port)
+    
+if __name__ == "__main__":
+    manager.run()
+    
+    
