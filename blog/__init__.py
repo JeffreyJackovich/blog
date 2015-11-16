e49f1b87c2d709330fb282da7fc30c0bad22cc7e
brooklynftw <jeffrey.jackovich@gmail.com>
2015-11-16 05:41:45 +0000

    MVP - About me blog

--- blog/__init__.py
+++ blog/__init__.py
@@ -5,5 +5,11 @@ app = Flask(__name__)
 config_path = os.environ.get("CONFIG_PATH", "blog.config.DevelopmentConfig")
 app.config.from_object(config_path)
 
-from . import view
+from . import login
+from . import views
 from . import filters
+
+
+
+
+
